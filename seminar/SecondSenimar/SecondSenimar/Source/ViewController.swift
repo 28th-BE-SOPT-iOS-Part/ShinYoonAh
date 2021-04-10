890038ee5bf96ebc5073bcb0c1c169af04a7fdd0
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-04-10 20:00:07 +0900

    [Add] 두번째 세미나 자료 추가

---
+++ seminar/SecondSenimar/SecondSenimar/Source/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SecondSenimar
+//
+//  Created by SHIN YOON AH on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
